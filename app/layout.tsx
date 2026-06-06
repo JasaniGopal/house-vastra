93a340e6fc93868e5c678d8af89d4b2f87defcc9
Gopal Jasani <gopal@Gopals-MacBook-Air.local>
2026-06-06 18:02:42 +0530

    login and register routes set up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Bodoni_Moda, Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.scss";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 
 const bodoniModa = Bodoni_Moda({
   variable: "--font-bodoni-moda",
@@ -32,12 +30,9 @@ export default function RootLayout({
       className={`${bodoniModa.variable} ${plusJakartaSans.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-[#fcf9f8] text-[#1c1b1b] font-sans">
-        <Header />
-        <main className="flex-1 flex flex-col pt-[72px]">
-          {children}
-        </main>
-        <Footer />
+        {children}
       </body>
     </html>
   );
 }
+
